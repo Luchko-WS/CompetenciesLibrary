e4db4e4a3b753891169d008df9e4cb8ccbbb55d7
Luchko-WS <andriyluchko@gmail.com>
2017-04-30 01:09:03 +0300

    read from DB

--- app/main.js
+++ app/main.js
@@ -1,23 +1,14 @@
 var mainApp = angular.module('MainApp', ['ngRoute', 'ngResource']);
 
-var Parameters = {
-    cityName: 'ALL',
-    minDate : '01.01.2016',
-    maxDate : '01.01.2017'
-};
 var DataFromDB = 'empty';
-var DataFromAPI = 'empty';
 
 mainApp.config(['$routeProvider', function ($routeProvider){
     $routeProvider
-        .when('/showFromDB', {
-            templateUrl: 'views//showFromDB.html',
-            controller: ''
-        })
-        .when('/forecast', {
-            templateUrl: 'views//forecast.html'
+        .when('/edit', {
+            templateUrl: 'views//editView.html'
         })
         .otherwise({
-            redirectTo: '/'
+            templateUrl: 'views//mainView.html',
+            controller: ''
         });
 }]);
